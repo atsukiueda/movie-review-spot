941a37266576d0ec0843f174ee714b90a9c85ff6
atsukiueda <atsuki.ueda.npu@gmail.com>
2019-01-26 07:22:02 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <div class="center jumbotron">
         <div class="text-center">
             <h1>タイトル</h1>
+            {!! link_to_route('signup.get', 'ユーザー登録', [], ['class' => 'btn btn-outline-primary btn-lg']) !!}
         </div>
     </div>
 @endsection
