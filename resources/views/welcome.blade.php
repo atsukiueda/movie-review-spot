81cef6a3296136241000ab93433d77cbaa7f6a63
atsukiueda <atsuki.ueda.npu@gmail.com>
2019-01-26 08:05:05 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,18 @@
     <div class="center jumbotron">
         <div class="text-center">
             <h1>タイトル</h1>
-            {!! link_to_route('signup.get', 'ユーザー登録', [], ['class' => 'btn btn-outline-primary btn-lg']) !!}
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-2 mt-sm-5">
+            @if (Auth::check())
+                {!! link_to_route('logout.get', 'ログアウト', [], ['class' => 'btn btn-outline-warning btn-lg']) !!}
+            @else
+                {!! link_to_route('login', 'ログイン', [], ['class' => 'btn btn-outline-success btn-lg']) !!}
+                <div class="mt-sm-2">
+                {!! link_to_route('signup.get', 'ユーザー登録', [], ['class' => 'btn btn-outline-primary btn-lg']) !!}
+                </div>
+            @endif
         </div>
     </div>
 @endsection
